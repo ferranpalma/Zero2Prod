4dbea63bf543407ae208c26649238177b2356932
ferranpalma <ferranpalmac@gmail.com>
2023-11-09 18:08:31 +0100

    Duplicated listener fixed

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
+    run(listener)?.await
 }
