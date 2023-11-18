f883e3fa5cdfaeeff9b60cf5a9fee69634fddd9f
ferranpalma <ferranpalmac@gmail.com>
2023-11-18 11:35:14 +0100

    subscriber default sink is stdout

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use zero2prod::telemetry;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = telemetry::get_subscriber(String::from("zero2prod"), String::from("into"));
+    let subscriber = telemetry::get_subscriber(
+        String::from("zero2prod"),
+        String::from("into"),
+        std::io::stdout,
+    );
     telemetry::init_subscriber(subscriber);
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
